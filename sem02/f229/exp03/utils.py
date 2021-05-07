ac9df657627e807fcaf02b086d480e9547a29b8d
PeSader <0f983427-355c-4bc4-8ac0-9c0f3dce504a@anonaddy.me>
2021-05-07 10:12:05 -0300

    document better

--- sem02/f229/exp03/utils.py
+++ sem02/f229/exp03/utils.py
@@ -1,13 +1,13 @@
- import math
+from typing import List, Tuple
 
 def lefetivo_tubo(l: float, r: float, m: int) -> float:
     """
 
-    :param l: float: comprimento do tubo
-    :param r: float: raio do tubo
-    :param m: int: numero de aberturas no tubo
+    :param l: comprimento do tubo
+    :param r: raio do tubo
+    :param m: numero de aberturas no tubo
 
-    :return: float: comprimento efetivo do tubo
+    :return: comprimento efetivo do tubo
 
     """
     return l + 0.6*r*m
@@ -15,10 +15,10 @@ def lefetivo_tubo(l: float, r: float, m: int) -> float:
 def lefetivo_ressoador(l: float, r: float) -> float:
     """
 
-    :param l: float: comprimento do gargalo do ressoador
-    :param r: float: raio do gargalo do ressoador
+    :param l: comprimento do gargalo do ressoador
+    :param r: raio do gargalo do ressoador
 
-    :return: float: comprimento efetivo do gargalo do ressoador
+    :return: comprimento efetivo do gargalo do ressoador
 
     """
     return l + 1.45*r
@@ -26,7 +26,7 @@ def lefetivo_ressoador(l: float, r: float) -> float:
 def incerteza_triangular(v: float) -> float:
     """
 
-    :param v: float: variacao com relacao a melhor estimativa, por exemplo
+    :param v: variacao com relacao a melhor estimativa, por exemplo
         'esse objeto tem 20cm mais ou menos 1cm de altura' onde u=1cm
 
     :return: incerteza padrao associada a variacao dada para uma funcao de
@@ -38,7 +38,7 @@ def incerteza_triangular(v: float) -> float:
 def incerteza_quadrada(v: float) -> float:
     """
 
-    :param v: float: variacao com relacao a melhor estimativa, por exemplo
+    :param v: variacao com relacao a melhor estimativa, por exemplo
         'esse objeto tem 20cm mais ou menos 1cm de altura' onde u=1cm
 
     :return: incerteza padrao associada a variacao dada para uma funcao de
@@ -46,3 +46,19 @@ def incerteza_quadrada(v: float) -> float:
 
     """
     return v/(3**0.5)
+
+def incerteza_padrao(l: List[Tuple[float, float]]) -> float:
+    """
+
+    :param l: lista de tuplas que descrevem a incerteza de um parametro
+        o primeiro elemento da tupla deve ser o coeficiente de sensibilidade
+        e o segundo elemento da tupla deve ser a incerteza padrao
+
+    :return: incerteza padrao combinada para uma grandeza
+
+    """
+    result_squared = 0
+    for i in l:
+        result_squared += i[0]**2 * i[1]**2
+    result = result_squared ** (1/2)
+    return result
