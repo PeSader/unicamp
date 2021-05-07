7c13a4946d3dc6af8054fea87990163591bc68e1
PeSader <0f983427-355c-4bc4-8ac0-9c0f3dce504a@anonaddy.me>
2021-05-06 21:58:36 -0300

    adicionar principais constantes

---
+++ sem02/f229/exp03/constants.py
@@ -0,0 +1,19 @@
+from uncertainties import ufloat
+
+VELOCIDADE_DO_SOM_ESPERADA = 340.27 #(m/s)
+
+# Oscilador (celular com phyphox)
+FREQUENCIA_MINIMA_PHYPHOX = 10 #(Hz)
+FREQUENCIA_MAXIMA_PHYPHOX = 10000 #(Hz)
+
+# Experimento dos tubos
+COMPRIMENTO_TUBO_PEQUENO = 0.225 #(m)
+DIAMETRO_TUBO_PEQUENO = 0.037 #(m)
+
+COMPRIMENTO_TUBO_GRANDE = 1.001 #(m)
+DIAMETRO_TUBO_GRANDE = 0.037 #(m)
+
+# Experimento do Ressoador de Helmholtz
+COMPRIMENTO_GARGALO = 0.10 #(m)
+DIAMETRO_GARGALO = 0.025 #(m)
+VOLUME_DO_RESSOADOR = 0.00076 #(m**3)
