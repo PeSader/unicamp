9acaa8d3627f9c055677c4825994f313c42625f7
PeSader <0f983427-355c-4bc4-8ac0-9c0f3dce504a@anonaddy.me>
2021-05-06 22:03:25 -0300

    arrumar indentacao

--- sem02/f229/exp03/utils.py
+++ sem02/f229/exp03/utils.py
@@ -7,7 +7,7 @@ def lefetivo_ressoador(l: float, r: float) -> float:
     return l + 1.45*r
 
 def incerteza_triangular(u: float) -> float:
-   return u/(6**0.5)
+    return u/(6**0.5)
 
 def incerteza_quadrada(u: float) -> float:
-   return u/(3**0.5)
+    return u/(3**0.5)
